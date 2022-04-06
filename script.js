41e1b0550479de353a3ecee6b0bf35d9ebffc494
Jun Kato <i@junkato.jp>
2022-04-06 23:44:51 +0900

    Remove comments

--- script.js
+++ script.js
@@ -1,58 +1,35 @@
-/**
- * TextAlive App API lyric sheet example
- * https://github.com/TextAliveJp/textalive-app-lyric-sheet
- *
- * インタラクティブな歌詞カードを実装した TextAlive App API のサンプルコードです。
- * 発声にあわせて歌詞が表示され、歌詞をクリックするとそのタイミングに再生がシークします。
- * また、このアプリが TextAlive ホストと接続されていなければ再生コントロールを表示します。
- */
 const { Player } = TextAliveApp;
-
-// TextAlive Player を初期化
 const player = new Player({
-  // トークンは https://developer.textalive.jp/profile で取得したものを使う
   app: { token: "JY0mLoHiX3lPTJaS", parameters: [
     {title: "背景グラデーション開始色", name: "gradationStartColor", className: "Color", iniitalValue: "#63d0e2" },
     {title: "背景グラデーション終了色", name: "gradationEndColor", className: "Color", iniitalValue: "#ff9438" },
   ] },
-
   mediaElement: document.querySelector("#media"),
   mediaBannerPosition: "bottom right",
-
-  // オプション一覧
-  // https://developer.textalive.jp/packages/textalive-app-api/interfaces/playeroptions.html
 });
-
 const overlay = document.querySelector("#overlay");
 const bar = document.querySelector("#bar");
 const textContainer = document.querySelector("#text");
 const seekbar = document.querySelector("#seekbar");
 const paintedSeekbar = seekbar.querySelector("div");
 let b, c;
-
 player.addListener({
-  /* APIの準備ができたら呼ばれる */
   onAppReady(app) {
     if (app.managed) {
       document.querySelector("#control").className = "disabled";
     }
     if (!app.songUrl) {
       document.querySelector("#media").className = "disabled";
-
       player.createFromSongUrl("https://piapro.jp/t/FDb1/20210213190029", {
         video: {
-          // 音楽地図訂正履歴: https://songle.jp/songs/2121525/history
           beatId: 3953882,
           repetitiveSegmentId: 2099561,
-          // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FFDb1%2F20210213190029
           lyricId: 52065,
           lyricDiffId: 5093,
         },
       });
     }
   },
-
-  /* パラメタが更新されたら呼ばれる */
   onAppParameterUpdate: () => {
     const params = player.app.options.parameters;
     const sc = player.app.parameters.gradationStartColor, scString = sc ? `rgb(${sc.r}, ${sc.g}, ${sc.b})` : params[0].iniitalValue;
@@ -60,41 +37,26 @@ player.addListener({
     document.body.style.backgroundColor = ecString;
     document.body.style.backgroundImage = `linear-gradient(0deg, ${ecString} 0%, ${scString} 100%)`;
   },
-
-  /* 楽曲が変わったら呼ばれる */
   onAppMediaChange() {
-    // 画面表示をリセット
     overlay.className = "";
     bar.className = "";
     resetChars();
   },
-
-  /* 楽曲情報が取れたら呼ばれる */
   onVideoReady(video) {
-    // 楽曲情報を表示
     document.querySelector("#artist span").textContent =
       player.data.song.artist.name;
     document.querySelector("#song span").textContent = player.data.song.name;
-
-    // 最後に表示した文字の情報をリセット
     c = null;
   },
-
-  /* 再生コントロールができるようになったら呼ばれる */
   onTimerReady() {
     overlay.className = "disabled";
     document.querySelector("#control > a#play").className = "";
     document.querySelector("#control > a#stop").className = "";
   },
-
-  /* 再生位置の情報が更新されたら呼ばれる */
   onTimeUpdate(position) {
-    // シークバーの表示を更新
     paintedSeekbar.style.width = `${
       parseInt((position * 1000) / player.video.duration) / 10
     }%`;
-
-    // 現在のビート情報を取得
     let beat = player.findBeat(position);
     if (b !== beat) {
       if (beat) {
@@ -107,21 +69,14 @@ player.addListener({
       }
       b = beat;
     }
-
-    // 歌詞情報がなければこれで処理を終わる
     if (!player.video.firstChar) {
       return;
     }
-
-    // 巻き戻っていたら歌詞表示をリセットする
     if (c && c.startTime > position + 1000) {
       resetChars();
     }
-
-    // 500ms先に発声される文字を取得
     let current = c || player.video.firstChar;
     while (current && current.startTime < position + 500) {
-      // 新しい文字が発声されようとしている
       if (c !== current) {
         newChar(current);
         c = current;
@@ -129,23 +84,17 @@ player.addListener({
       current = current.next;
     }
   },
-
-  /* 楽曲の再生が始まったら呼ばれる */
   onPlay() {
     const a = document.querySelector("#control > a#play");
     while (a.firstChild) a.removeChild(a.firstChild);
     a.appendChild(document.createTextNode("\uf28b"));
   },
-
-  /* 楽曲の再生が止まったら呼ばれる */
   onPause() {
     const a = document.querySelector("#control > a#play");
     while (a.firstChild) a.removeChild(a.firstChild);
     a.appendChild(document.createTextNode("\uf144"));
   },
 });
-
-/* 再生・一時停止ボタン */
 document.querySelector("#control > a#play").addEventListener("click", (e) => {
   e.preventDefault();
   if (player) {
@@ -157,21 +106,15 @@ document.querySelector("#control > a#play").addEventListener("click", (e) => {
   }
   return false;
 });
-
-/* 停止ボタン */
 document.querySelector("#control > a#stop").addEventListener("click", (e) => {
   e.preventDefault();
   if (player) {
     player.requestStop();
-
-    // 再生を停止したら画面表示をリセットする
     bar.className = "";
     resetChars();
   }
   return false;
 });
-
-/* シークバー */
 seekbar.addEventListener("click", (e) => {
   e.preventDefault();
   if (player) {
@@ -181,14 +124,7 @@ seekbar.addEventListener("click", (e) => {
   }
   return false;
 });
-
-/**
- * 新しい文字の発声時に呼ばれる
- * Called when a new character is being vocalized
- */
 function newChar(current) {
-  // 品詞 (part-of-speech)
-  // https://developer.textalive.jp/packages/textalive-app-api/interfaces/iword.html#pos
   const classes = [];
   if (
     current.parent.pos === "N" ||
@@ -197,13 +133,9 @@ function newChar(current) {
   ) {
     classes.push("noun");
   }
-
-  // フレーズの最後の文字か否か
   if (current.parent.parent.lastChar === current) {
     classes.push("lastChar");
   }
-
-  // 英単語の最初か最後の文字か否か
   if (current.parent.language === "en") {
     if (current.parent.lastChar === current) {
       classes.push("lastCharInEnglishWord");
@@ -211,12 +143,8 @@ function newChar(current) {
       classes.push("firstCharInEnglishWord");
     }
   }
-
-  // noun, lastChar クラスを必要に応じて追加
   const div = document.createElement("div");
   div.appendChild(document.createTextNode(current.text));
-
-  // 文字を画面上に追加
   const container = document.createElement("div");
   container.className = classes.join(" ");
   container.appendChild(div);
@@ -225,11 +153,6 @@ function newChar(current) {
   });
   textContainer.appendChild(container);
 }
-
-/**
- * 歌詞表示をリセットする
- * Reset lyrics view
- */
 function resetChars() {
   c = null;
   while (textContainer.firstChild)
